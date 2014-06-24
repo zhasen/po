5ddcb02f1ea6a8e2847fb79eaadcf46b53b13fe5
henryleu <henryleu@126.com>
2014-06-24 12:01:54 +0800

    add User model definition

--- source/models/User.js
+++ source/models/User.js
@@ -1,26 +1,31 @@
-var domainRegistry = require('./DomainRegistry');
+var Sequelize = require('sequelize');
+var sequelize = require('../commons/sequelize');
 var typeRegistry = require('./TypeRegistry');
-var db = domainRegistry.domainBuilder();
-db.withName('User')
-    .withBasis() //it includes id and version (may be version)
-    .withOptions({
-        modelKey: 'usr',
-        modelTable: 'user'
-    })
-    .withProperties({
-        lifeFlag: {type: String, defaultValue: typeRegistry.LifeFlag.Active.value()},
-        type: {type: String, defaultValue: typeRegistry.UserType.OAuth.value()},
-        displayName: {type: String, defaultValue: ''},
-        username: {type: String},
-        password: {type: String},
-        email: {type: String},
-        phone: {type: String},
-        userId: {type: String},
-        accessToken: {type: String},
-        refreshToken: {type: String},
-        expiresIn: {type: Date}
-    })
-    .withCreatedOn()
-    .withUpdatedOn();
 
-module.exports = db.build();
+var User = sequelize.define('User',
+    {
+        id: Sequelize.STRING,
+        lifeFlag: {
+            type: Sequelize.ENUM,
+            values: typeRegistry.LifeFlag.valueList(),
+            defaultValue: typeRegistry.LifeFlag.Active.value()
+        },
+        type: {
+            type: Sequelize.ENUM,
+            values: typeRegistry.UserType.valueList(),
+            defaultValue: typeRegistry.UserType.OAuth.value()
+        },
+        displayName: {type: Sequelize.STRING},
+        username: {type: Sequelize.STRING},
+        password: {type: Sequelize.STRING},
+        email: {type: Sequelize.STRING},
+        phone: {type: Sequelize.STRING},
+        accessToken: {type: Sequelize.STRING},
+        refreshToken: {type: Sequelize.STRING},
+        expiresIn: {type: Sequelize.DATE}
+    },
+    {
+        tableName: 'user'
+    }
+);
+module.exports = User;
