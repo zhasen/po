3dbd86b43a576257d976973fdb9957f8f5b7ed06
liugenjie <liuegnjie@gmail.com>
2014-06-26 16:49:39 +0800

    remove files about testquestion

--- source/routes/main.js
+++ source/routes/main.js
@@ -1,5 +1,6 @@
 var logger = require('../commons/logging').logger;
 var util = require('util');
+var PageInput = require('./common/PageInput');
 var DictService = require('../services/DictService');
 
 module.exports = function (app) {
@@ -46,33 +47,9 @@ module.exports = function (app) {
         next();
     };
 
-    //为试卷列表查询数据并传输到模板
-//    var testPaperIndexPage = function (req, res, next) {
-//        asseton(req, res);
-//        var input = {};
-//        var page = {};
-//        input.page = page;
-//        page.user = req.user;
-//        page.papers = {};
-//        page.papers.paperType = Papers.paperType;
-//        page.papers.paperPublish = Papers.paperPublish;
-//        page.papers.testArea = Papers.testArea;
-//        TestPapersService.listTestPapers(function (err, list) {
-//            if (err) {
-//                logger.error(err);
-//                res.json(500, err); //TODO response a json document with error info
-//                return;
-//            }
-//            page.testPapers = list;
-//            console.log(input);
-//            res.render('tp-index', input);
-//        });
-//    };
-
-
     //练习包列表查询数据并传输到模板
-    /*var ssoLoginCallback = function (req, res, next) {
-        asseton(req, res);
+    var ssoLoginCallback = function (req, res, next) {
+        /*asseton(req, res);
         var input = {};
         var page = {};
         input.page = page;
@@ -90,15 +67,19 @@ module.exports = function (app) {
             page.testPapers = list;
             console.log(input);
             res.render('tep-list', input);
-        });
-    };*/
-    //app.get('/u2/callback', ssoLoginCallback);
+        });*/
+    };
+    app.get('/u2/callback', ssoLoginCallback);
 
     app.get('/', dictPage);
     app.get('/tq-index', initUser);
     app.get('/tqt-index', initUser);
     app.get('/tq-edit', initUser);
 
-//    require("./design")(app);
-    require("./testQuestion")(app);
+    app.get('/course-schedule', function (req, res, next) {
+        asseton(req, res);
+        var input = PageInput.i().enums();
+        res.render('course-schedule', input);
+    }); // 试验课表EJS，正式使用时不需要此路由
+
 };
