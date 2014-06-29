7ae140d8747add8d9c4eadef6fddfbd7d6b24dec
henryleu <henryleu@126.com>
2014-06-29 12:07:19 +0800

    remove useless routing file

--- source/routes/index.js
+++ source/routes/index.js
@@ -1,12 +1,6 @@
 var main = require("./main")
-    ,recordWebSocket = require("./recordWebSocket"),
-    testUser = require("./testUser");
-    testPackage = require("./testPackage");
-testPackage.js = require("./testPackage");
+    ,recordWebSocket = require("./recordWebSocket");
 module.exports = function(app) {
     main(app);
     recordWebSocket(app);
-    testUser(app);
-    testPackage(app);
-    testPackage(app);
 };
